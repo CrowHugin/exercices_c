d22786801a8bc20fe10926aefdef08e265505a20
CrowHugin <kuntz.bastien@gmail.com>
2025-07-31 00:18:17 +0200

    ex3

--- exercices_simples_electro_info/ex_simples.c
+++ exercices_simples_electro_info/ex_simples.c
@@ -22,17 +22,39 @@ Même chose que l'exercice 1 avec la division à la place de la multiplication.
 Vérifier que le 2ème nombre est différent de zéro, sinon afficher "Erreur : division par 0 !".*/
 
 
+// #include <stdio.h>
+
+// int main(){
+//     int int1, int2;
+//     int nb3;
+//     scanf("%d%d",&int1,&int2);
+//     nb3 = int1/int2;
+//     if (nb3 != 0){
+//         printf("%d/%d=%d\n",int1, int2, nb3);
+//     }else{
+//         printf("Erreur : division par 0 !\n");
+//     }
+//     return 0;
+// }
+
+/*Exercice 3
+Demander à l’utilisateur de saisir un nombre entier.
+
+Afficher si ce nombre est pair ou impair (le reste de la division entière de ce nombre par deux égal à 0 ou non).
+
+Pour obtenir le reste d'une division, on utilise l'opérateur "Modulo". En langage C l'opérateur Modulo est représenté par le symbole % 
+*/
+
 #include <stdio.h>
 
 int main(){
-    int int1, int2;
-    int nb3;
-    scanf("%d%d",&int1,&int2);
-    nb3 = int1/int2;
-    if (nb3 != 0){
-        printf("%d/%d=%d\n",int1, int2, nb3);
+    int nb1,nb2,nb3;
+    scanf("%i%i", &nb1,&nb2);
+    nb3 = nb1%nb2;
+    if (nb3!=0){
+        printf("Le nombre %i est impair\n", nb3);
     }else{
-        printf("Erreur : division par 0 !\n");
+        printf("Le nombre %i est pair\n", nb3);
     }
-    return 0;
+
 }
